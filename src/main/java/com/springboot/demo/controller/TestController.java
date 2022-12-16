6ff56768c35bc479cdda138d66e9b4f8e50e33f0
Root310 <rahil.inexture@gmail.com>
2022-12-16 12:33:51 +0530

    added slf4j logger

--- src/main/java/com/springboot/demo/controller/TestController.java
+++ src/main/java/com/springboot/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/test-api")
     public ResponseEntity<String> testApi(){
+        logger.info("calling... test-api");
         return ResponseEntity.ok("Success 200 code");
     }
 }
